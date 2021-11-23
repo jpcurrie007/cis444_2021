e4a3374ee7f36e967fd1406f3961dfd076f3a4e2
John Paul Currie <curriejohnpaul@gmail.com>
2021-11-22 18:41:03 -0800

    added buy books functionality

---
+++ flask_jwt_rest_server/secure_calls/buyBook.py
@@ -0,0 +1,27 @@
+from flask import request, g
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from tools.token_tools import create_token
+import datetime
+
+from tools.logging import logger
+
+from db_con import get_db
+global_db_con = get_db()
+
+def handle_request():
+    logger.debug("Buy Book Handle Request")
+    cur = global_db_con.cursor()
+    bookName = request.form['book']
+    #print(bookName)
+    timeOfPurchase = datetime.datetime.now()
+    #print(timeOfPurchase)
+    dbCmdForBuyingBook = "INSERT INTO booksBought(bookName, time) VALUES('"
+    dbCmdForBuyingBook += str(bookName)
+    dbCmdForBuyingBook += "','"
+    dbCmdForBuyingBook += str(timeOfPurchase)
+    dbCmdForBuyingBook += "');"
+    #print(dbCmdForBuyingBook)
+    cur.execute(dbCmdForBuyingBook)
+    global_db_con.commit()
+
+    return json_response(token = create_token(g.jwt_data) , message = "Thank you for buying one of my books :)")
