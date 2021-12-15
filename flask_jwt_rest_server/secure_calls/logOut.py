aa24e30c84dff765af57352306db810f99ae17e3
John Paul Currie <curriejohnpaul@gmail.com>
2021-12-15 11:22:37 -0800

    added chat refresh

--- flask_jwt_rest_server/secure_calls/logOut.py
+++ flask_jwt_rest_server/secure_calls/logOut.py
@@ -1,7 +1,6 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
-import datetime
 
 from tools.logging import logger
 
